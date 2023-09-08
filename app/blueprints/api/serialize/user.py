952c6c9802e25c0cbf326fdcc2b0fbc264d74a64
bagussr <bagusssyamsu@gmail.com>
2023-09-08 07:58:33 +0700

    some uppdate

--- app/blueprints/api/serialize/user.py
+++ app/blueprints/api/serialize/user.py
@@ -3,10 +3,8 @@ from marshmallow import Schema, fields
 
 class UserSchema(Schema):
     id = fields.Integer()
+    name = fields.String()
     username = fields.String()
     password = fields.String()
     created_date = fields.DateTime()
     updated_date = fields.DateTime()
-
-    class Meta:
-        fields = ("id", "username", "password", "created_date", "updated_date")
